d36c7373488f3dfa05cc3d2a6550741170dc8feb
Sten Roger Sandvik <srs@enonic.com>
2016-04-15 10:36:35 +0200

    Added reactjs.

--- src/proto/js/main.jsx
+++ src/proto/js/main.jsx
@@ -1,25 +1,37 @@
-// Namespace for our app
-var app = {};
-
-// Countdown timer
-app.countdownTimer = React.createClass({
-    getInitialState: function () {
-        return {elapsed: 0};
-    },
-    tick: function () {
-        this.setState({elapsed: new Date() - this.props.start});
-    },
-    componentDidMount: function () {
-        this.timer = setInterval(this.tick, 50);
-    },
-    componentWillUnmount: function () {
-        clearInterval(this.timer);
-    },
+const HomeView = React.createClass({
     render: function () {
-        var elapsed = Math.round(this.state.elapsed / 100);
-        var seconds = (elapsed / 10).toFixed(1);
-        return <p>Started <b>{seconds} seconds</b> ago.</p>;
+        return <p>Home</p>;
     }
 });
 
-ReactDOM.render(<app.countdownTimer start={Date.now()}/>, $('#main').get(0));
+const AboutView = React.createClass({
+    render: function () {
+        return <p>About</p>;
+    }
+});
+
+const App = React.createClass({
+    getInitialState: function () {
+        return {
+            route: window.location.hash.substr(1)
+        }
+    },
+    componentDidMount: function () {
+        window.addEventListener('hashchange', () => {
+            this.setState({
+                route: window.location.hash.substr(1)
+            })
+        })
+    },
+    render: function () {
+        if (this.state.route == 'about') {
+            return <AboutView/>;
+        }
+
+        return <HomeView/>;
+    }
+});
+
+$(function () {
+    window.app = ReactDOM.render(<App/>, $('#main').get(0));
+});
