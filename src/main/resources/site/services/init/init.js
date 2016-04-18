facc2be36b4347d6d50cb6936febf575ec1ef056
Alex Rodriguez <aro@enonic.com>
2016-04-18 13:22:12 +0200

    Send available games on page load

--- src/main/resources/site/services/init/init.js
+++ src/main/resources/site/services/init/init.js
@@ -1,10 +1,12 @@
 var portalLib = require('/lib/xp/portal');
+var contentLib = require('/lib/xp/content');
 
 exports.get = function (req) {
     var wsUrl = portalLib.serviceUrl({service: 'gameHub', type: 'absolute'});
     wsUrl = 'ws' + wsUrl.substring(wsUrl.indexOf(':'));
     var data = {
-        wsUrl: wsUrl
+        wsUrl: wsUrl,
+        games: getGames()
     };
 
     return {
@@ -12,3 +14,28 @@ exports.get = function (req) {
         body: "var xphoot_data = " + JSON.stringify(data, null, 2) + ";"
     }
 };
+
+function getGames() {
+    var result = contentLib.query({
+        start: 0,
+        count: 1000,
+        sort: "modifiedTime DESC",
+        contentTypes: [
+            app.name + ":game"
+        ]
+    });
+
+    log.info('Games: %s', JSON.stringify(result, null, 4));
+
+    var games = [], i, game, results = result.hits, res;
+    for (i = 0; i < results.length; i++) {
+        res = results[i];
+        game = {};
+        game.id = res._id;
+        game.name = res.displayName;
+        game.questions = res.data.questions.length;
+        games.push(game);
+    }
+
+    return games;
+}
