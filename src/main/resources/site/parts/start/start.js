7672b7524dc322eefd825398d8a3fe119a1714a0
Runar Myklebust <rmy@enonic.com>
2016-05-12 15:59:22 +0200

    XP-3373 Make XpHoot ready for market
    Fixed url's

--- src/main/resources/site/parts/start/start.js
+++ src/main/resources/site/parts/start/start.js
@@ -1,12 +1,24 @@
-var portal = require('/lib/xp/portal'); // Import the portal functions
+var portalLib = require('/lib/xp/portal'); // Import the portal functions
 var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
 
 // Handle the GET request
 exports.get = function (req) {
 
+
+    var branch = req.branch;
+
+    var masterUrl = portalLib.url({
+        path: '/portal/' + branch + '/xphoot/master'
+    });
+
+    var playerUrl = portalLib.url({
+        path: '/portal/' + branch + '/xphoot/player'
+    });
+
     // Prepare the model object with the needed data from the content
     var model = {
-        fisk: 'ost'
+        masterUrl: masterUrl,
+        playerUrl: playerUrl
     };
 
     // Specify the view file to use
