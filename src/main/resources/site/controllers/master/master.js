d89683484268c2e20f5411e57ecb5a1c09e1ccbd
Alex Rodriguez <aro@enonic.com>
2016-08-24 15:08:20 +0200

    Make the Join button more obvious for players

--- src/main/resources/site/controllers/master/master.js
+++ src/main/resources/site/controllers/master/master.js
@@ -1,15 +1,16 @@
 var thymeleafLib = require('/lib/xp/thymeleaf');
 var portalLib = require('/lib/xp/portal');
 
+var view = resolve('master.html');
+
 exports.get = function (req) {
-    var view = resolve('master.html');
     var siteConfig = portalLib.getSiteConfig();
     var url;
 
     if (siteConfig.playerUrl) {
         url = siteConfig.playerUrl;
     } else {
-        url = portalLib.pageUrl({path: portalLib.getSite()._path + '/player', type: 'absolute'});
+        url = portalLib.pageUrl({path: portalLib.getSite()._path, type: 'absolute'});
     }
     url = url.indexOf('://') > 0 ? url.substring(url.indexOf('://') + 3) : url;
     var params = {
--- src/main/resources/site/controllers/player/player.js
+++ src/main/resources/site/controllers/player/player.js
@@ -1,7 +1,8 @@
 var thymeleafLib = require('/lib/xp/thymeleaf');
 
+var view = resolve('player.html');
+
 exports.get = function (req) {
-    var view = resolve('player.html');
     var params = {};
 
     var body = thymeleafLib.render(view, params);
